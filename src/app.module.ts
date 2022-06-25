ce721340f71f5db3ccddf2934a665b8ad41149c4
KarenRamos2 <89101028+KarenRamos2@users.noreply.github.com>
2022-06-25 11:03:03 -0500

    feat(users): added users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { PatientsModule } from './patients/patients.module';
 import { MedicsModule } from './medics/medics.module';
 import { DiagnosticsModule } from './diagnostics/diagnostics.module';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { ProfileModule } from './profile/profile.module';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
   imports: [
@@ -16,6 +18,8 @@ import { AppointmentsModule } from './appointments/appointments.module';
     MedicsModule,
     DiagnosticsModule,
     AppointmentsModule,
+    ProfileModule,
+    SharedModule,
   ],
   controllers: [AppController],
   providers: [AppService],
