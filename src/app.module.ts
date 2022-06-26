dfdf235ccd5354b90413f34c522f35fd0255b9ff
RenatoPugaR <u202018303@upc.edu.pe>
2022-06-26 15:59:44 -0500

    First Commit of Appointments

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { PatientsModule } from './patients/patients.module';
+import { MedicsModule } from './medics/medics.module';
+import { DiagnosticsModule } from './diagnostics/diagnostics.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [
+    TypeOrmModule.forRoot(),
+    UsersModule,
+    PatientsModule,
+    MedicsModule,
+    DiagnosticsModule,
+    AppointmentsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
