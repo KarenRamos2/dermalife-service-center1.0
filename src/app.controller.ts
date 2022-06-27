06305b93e24a89e7dbcaa003ee8095b99a2d971e
RenatoPugaR <u202018303@upc.edu.pe>
2022-06-26 19:30:35 -0500

    Fourth Commit of Feature-Appointments

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
+
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { AppService } from "./AppService";
 
 @Controller()
 export class AppController {
